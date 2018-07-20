8b3cdabd44cf9beceee5182ce6911718d9d90528
Mig Gebril <migdam.gebril@gmail.com>
2018-07-19 20:28:46 -0400

    Sign in forms and global app properties

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { promiseMiddleware } from './middleware';
 
 const defaultState = {
-  appName: 'conduit',
+  appName: 'Pickup',
   articles: null
 };
 const reducer = function(state = defaultState, action) {
